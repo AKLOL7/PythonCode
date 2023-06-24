a0fc5ee612306cd5af580fb856b6a4194a2901f4
Sudeep Prasad <sudeep.rp@gmail.com>
2023-06-24 16:23:10 +0530

    add units

--- TempConversion.py
+++ TempConversion.py
@@ -2,9 +2,9 @@ t = float(input(" What is the measure of the degrees: "))
 p = input(" Is it in celsius(c) or fahrenheit(f): ")
 if p.lower() == "c":
     t = (t*(9/5)) + 32
-    print(t)
+    print(f'{t} fahrenheit')
 elif p.lower() == "f":
     t = (t-32) * (5/9)
-    print(t)
+    print(f'{t} celcis')
 else:
     print(" I don't understand that, sorry. ")
